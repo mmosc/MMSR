f75667dd98142943356a68b7bc1ffbf589744d06
Luke100000 <jonas.peche@aon.at>
2023-04-25 22:04:15 +0200

    added overlap similarity

--- src/similarity_functions.py
+++ src/similarity_functions.py
@@ -12,10 +12,14 @@ def get_cosine_similarity(arr_a: np.array, arr_b: np.array) -> np.array:
 def get_jaccard_similarity(arr_a: np.array, arr_b: np.array) -> np.array:
     norms_a = np.linalg.norm(arr_a, axis=-1)[:, np.newaxis]
     norms_b = np.linalg.norm(arr_b, axis=-1)[:, np.newaxis]
-    divisor = norms_a**2 + norms_b.T**2
+    divisor = norms_a ** 2 + norms_b.T ** 2
     dot_p = arr_a @ arr_b.T
     return dot_p / (divisor - dot_p)
 
 
 def get_inner_product_similarity(arr_a: np.array, arr_b: np.array) -> np.array:
     return arr_a @ arr_b.T
+
+
+def get_overlap_similarity(arr_a: np.array, arr_b: np.array) -> np.array:
+    return arr_a @ arr_b.T > 0
